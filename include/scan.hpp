a27601cd0427c6226556e592b630a80f1e6bea0d
Dima <dima@MacBook-Air-admin.local>
2026-05-13 11:29:44 +0300

    Fixed the specified errors

--- include/scan.hpp
+++ include/scan.hpp
@@ -18,14 +18,12 @@ std::expected<details::scan_result<Ts...>, details::scan_error> scan(std::string
     if (input_parts.size() != sizeof...(Ts)) {
         return std::unexpected(details::scan_error{"Number of input parts does not match number of types"});
     }
-
     std::tuple<std::expected<Ts, details::scan_error>...> results;
     [&]<size_t... Is>(std::index_sequence<Is...>) {
         ((std::get<Is>(results) = details::parse_value_with_format<std::tuple_element_t<Is, std::tuple<Ts...>>>(
               input_parts[Is], format_parts[Is])),
          ...);
     }(std::index_sequence_for<Ts...>{});
-
     details::scan_error err{""};
     auto check = [&](auto &&exp) {
         if (!exp.has_value()) {
@@ -34,12 +32,10 @@ std::expected<details::scan_result<Ts...>, details::scan_error> scan(std::string
         }
         return true;
     };
-
     bool all_ok = std::apply([&check](auto &&...exps) { return (check(exps) && ...); }, results);
     if (!all_ok) {
         return std::unexpected(err);
     }
-
     return std::apply(
         [](auto &&...exps) { return details::scan_result<Ts...>{std::tuple<Ts...>(std::move(exps.value())...)}; },
         results);
